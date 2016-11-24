4fd0bbe0cb4608aef98aea609a761a2021fa7e99
Michael Henson <mhenson@gmail.com>
2016-11-24 01:01:24 -0600

    Add files via upload

---
+++ src/tlc/ServerApp.java
@@ -0,0 +1,21 @@
+package tlc;
+
+public class ServerApp
+{
+    public static void main(final String[] args)
+    {
+//        final MHScreen screen = new TLCLoginScreen();
+//
+//        final MHVideoSettings settings = new MHVideoSettings();
+//        settings.displayWidth = 800;
+//        settings.displayHeight = 600;
+//        settings.fullScreen = false;
+//        settings.windowCaption = "Team Laser Combat Server Application";
+//
+//        TLCDataFacade.getInstance().setPlayerMode(TLCPlayerMode.STANDALONE_HOST);
+//
+//        new MHGameApplication(screen, settings);
+//
+//        System.exit(0);
+    }
+}
--- src/tlc/TLCMain.java
+++ src/tlc/TLCMain.java
@@ -1,1 +1,29 @@
+package tlc;
 
+import mhframework.MHAppLauncher;
+import mhframework.MHDisplayModeChooser;
+import mhframework.MHGameApplication;
+import mhframework.MHScreen;
+import mhframework.MHVideoSettings;
+import tlc.data.TLCDataFacade;
+import tlc.ui.screens.TLCMainMenuScreen;
+
+public class TLCMain
+{
+    public static final long DATA_ID = TLCDataFacade.generateDataID();
+
+    public static void main(final String[] args)
+    {
+        final MHScreen screen = new TLCMainMenuScreen();
+
+        final MHVideoSettings settings = new MHVideoSettings();
+        settings.fullScreen = MHAppLauncher.showDialog(MHDisplayModeChooser.getFrame(), true);// false;
+        settings.displayWidth = MHAppLauncher.getResolution().width;//800;
+        settings.displayHeight = MHAppLauncher.getResolution().height;//600;
+        settings.windowCaption = "Team Laser Combat";
+
+        new MHGameApplication(screen, settings);
+
+        System.exit(0);
+    }
+}
